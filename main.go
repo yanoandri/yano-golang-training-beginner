58b082595665e18085c6fcab665ed1dce783ad0b
yanoandri <yano@xendit.co>
2021-04-26 20:53:40 +0700

    add reason

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 func main() {
 	// TODO: task #1 - Why is it not working?
 	// changing the type from const into var
+	// the reason to change it because the value depends on env variables in a system
+	// so using var will be the best option
 	var task1 = os.Getenv("task_1")
 	if !reflect.DeepEqual("", task1) {
 		fmt.Println("Task #1 failed!")
 	}
 	// TODO: Challenge #1 - How to convert from any type into string
-	// use FsprintF method
+	// use FsprintF method to convert into string the simple way
 	var challenge1 interface{} // DO NOT CHANGE THE DATATYPE
 	challenge1 = 1
 	challenge1 = fmt.Sprintf("%v", challenge1)
