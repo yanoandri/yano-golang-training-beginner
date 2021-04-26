e74a767679e4c026d396f4d9dc5e8fdbba495d3e
yanoandri <yano@xendit.co>
2021-04-26 20:28:44 +0700

    daily-digest 1

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
 
 func main() {
-	fmt.Printf("Hello, world or 你好，世界 or Καλημέρα κόσμε or こんにちは世界\n")
+	// TODO: task #1 - Why is it not working?
+	// changing the type from const into var
+	var task1 = os.Getenv("task_1")
+	if !reflect.DeepEqual("", task1) {
+		fmt.Println("Task #1 failed!")
+	}
+	// TODO: Challenge #1 - How to convert from any type into string
+	// use FsprintF method
+	var challenge1 interface{} // DO NOT CHANGE THE DATATYPE
+	challenge1 = 1
+	challenge1 = fmt.Sprintf("%v", challenge1)
+	if !reflect.DeepEqual("1", challenge1) {
+		fmt.Println("Challenge #1 failed!")
+	}
+	fmt.Println("All passed!")
 }
